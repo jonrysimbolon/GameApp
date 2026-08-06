30992b40e52528881fc79a4245e3f90ef3e51076
Jonry Simbolon <jonrysimbolon19@gmail.com>
2026-08-06 22:42:16 +0700

    push

--- Module/Shared/Package.swift
+++ Module/Shared/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "Shared",
             dependencies: [
-                .product(name: "Domain", package: "GameAppIosDomain"),
+                .product(name: "Domain", package: "GameAppIosDomain")
             ]
         )
     ],
